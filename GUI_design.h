6aa0f03ba5fff9dca1176ba521a3672319c06cdd
bozhi90 <79579563+bozhi90@users.noreply.github.com>
2021-05-12 03:14:41 -0700

    Add files via upload

--- GUI_design.h
+++ GUI_design.h
@@ -7,9 +7,22 @@ void GUI_design (void) {
     int aX, aY;
 
     for (int i = 0; i < index_num; i++) {
-        aX = DataXYZ[data_num][i + 1][0] - DataXYZ[data_num][i][0];
-        aY = DataXYZ[data_num][i + 1][1] - DataXYZ[data_num][i][1];
+        aX = DataXYZ[data_num][i][0] > 500 || DataXYZ[data_num][i][0] < 500;
+        aY = DataXYZ[data_num][i][1] > 500 || DataXYZ[data_num][i][1] < 500;
 
-
+        if (aX == 1) {
+            if (aY == 0) {
+                der[i] = 1;
+            } else {
+                der[i] = 2;
+            }
+        } else {
+            if (aY == 0) {
+                der[i] = 3;
+            } else {
+                der[i] = 4;
+            }
+        }
     }
+
 }
